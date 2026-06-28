91f788072b521fc7c45f49f38690179947f02d4a
AryanHande13 <rnhande13@gmail.com>
2026-06-28 20:22:39 +0530

    Update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tasktracker-backend-m10i.onrender.com/api',
+  baseURL: 'https://tasktracker-backend-1kgi.onrender.com/api',
 });
 
 export default api;
